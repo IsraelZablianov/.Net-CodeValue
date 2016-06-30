4dbaf2c77f01c0fa04af94938d364e6d2dca355f
Israel Zablianov <123hartk123@gmail.com>
2016-06-30 16:59:43 +0300

    IsraelZablianov

--- Ex04/MailSystem/MailSystem/Program.cs
+++ Ex04/MailSystem/MailSystem/Program.cs
@@ -12,20 +12,17 @@ namespace MailSystem
         static void Main(string[] args)
         {
             MailManager mailManager = new MailManager();
-            mailManager.MailArrived += HandleMailArrived;
+            mailManager.MailArrived += (sender,  e) => {
+                Console.WriteLine(
+@"Title - {0}
+body - {1}", e.Title, e.Body);
+            };
             mailManager.SimulateMailArrived("Avi - Holon, you win 1000000$");
 
             Timer timer = new Timer(
             Irrelevant => { mailManager.SimulateMailArrived("Avi - Holon, you win 1000000$");}, null, 0, 1000);
 
             Console.ReadKey();
-        }
-
-        private static void HandleMailArrived(object sender, MailArrivedEventArgs e)
-        {
-            Console.WriteLine(
-@"Title - {0}
-body - {1}",e.Title, e.Body);
         }
     }
 }
