76ae746420aad996a84a4e8d1596164a90f6e054
Israel Zablianov <123hartk123@gmail.com>
2017-04-01 01:20:26 +0300

    Fixed Projects

---
+++ Advanced Ex05/Jobs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Part A
+            var job = new Job();
+            job.AddProcessToJob(Process.Start("Notepad"));
+            job.AddProcessToJob(Process.Start("Calc"));
+            Console.ReadLine();
+            job.Kill();
+
+            //Part B => After runnig you will see
+            //GC has disposed some of the object
+            //due to memory presure I geuss
+            for (int i = 0; i < 20; i++)
+            {
+                var jobObject = new Job();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
